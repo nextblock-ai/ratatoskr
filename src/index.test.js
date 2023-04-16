5b70aba432416549ada40c593f717980cbc68a62
Sebastian Schepis <sschepis@gmail.com>
2023-04-15 17:08:08 -0700

    updates to installer

--- src/index.test.js
+++
@@ -1,7 +0,0 @@
-const { files } = require("../index");
-
-describe("index", () => {
-    it("should load files and display them in the file list", () => {
-        // Add your test logic here
-    });
-});

5e269a223b5aec541748de083f5255b5771b161e
Sebastian Schepis <sschepis@gmail.com>
2023-04-16 17:30:20 -0700

    major updates

---
+++ src/code.js
@@ -0,0 +1,143 @@
+require("dotenv").config();
+
+const enquirer = require("enquirer");
+const fs = require("fs-extra");
+const path = require("path");
+const ora = require("ora");
+
+const { OpenAIApi, Configuration } = require('openai');
+const configuration = new Configuration({ apiKey: process.env.OPENAI_KEY });
+const openai = new OpenAIApi(configuration);
+
+// Replace this with your actual API key
+openai.apiKey = process.env.OPENAI_KEY;
+
+// Load all files in ./src (excluding __test__)
+async function loadFiles(shellPath) {
+    // get the cwd
+    const cwd = process.cwd();
+    const srcPath = path.join(cwd, shellPath);
+    let files = await fs.readdir(srcPath);
+    files = files
+        .filter((file) => !file.includes("__tests__"))
+        .map(async (file) => {
+            const content = await fs.readFile(path.join(srcPath, file), "utf-8");
+            if (content.includes('gpt-exclude: true')) {
+                console.log(`Excluding file ${file} from training data`)
+                return undefined;
+            }
+            return { name: file, content };
+        })
+        .filter((file) => file !== undefined)
+    let fileContents = await Promise.all(
+        files
+    );
+    fileContents = fileContents.filter((file) => file !== undefined);
+
+    return fileContents;
+}
+
+// Create an OpenAI conversation with the contents of the loaded files
+async function createConversation(files) {
+    // Add an initial message to inform the AI of its role and command capabilities
+    const initialMessage = {
+        role: "system",
+        content: `You are an AI assistant with regex-enabled search-and-replace functionality for modifying files. Your main role is to process and execute commands to edit the files. You are allowed to use control statements to guide the user or respond to user questions using the !echo statement.
+Shell statement format:
+!bash <command>
+
+- <command>: The command to execute in the shell, enclosed in quotes. Use shelljs conventions for escaping characters.
+
+You will be shown the results of the command execution. If the command is successful, you can output !success to acknowledge the success. If the command fails, you can output another commant to attempt to resolve the issue. If you cannot resolve the issue, output !failure to acknowledge the failure.
+Control statement format:
+!edit <file> <search_pattern> <replacement>
+
+- <file>: The target file name. You can create a new file by specifying a file name that does not exist.
+- <search_pattern>: The regex pattern to search for in the file, enclosed in quotes.
+- <replacement>: The replacement string, enclosed in quotes.
+
+Echo statement format:
+!echo <message>
+
+- <message>: The message to echo back to the user.
+
+Make sure to properly encode newlines and special characters in your response.`,
+    };
+
+    const messages = files.map((file) => ({
+        role: "system",
+        content: `The content of ${file.name}:\n${file.content}`,
+    }));
+
+    // Add the initial message to the conversation
+    messages.unshift(initialMessage);
+    messages.push({ role: "user", content: " **CRITICAL** RESPOND ONLY WITH SHELL, EDIT, ECHO, SUCCESS OR FAILURE STATEMENTS. THIS IS ESSENTIAL!!" });
+    return messages;
+}
+
+// Get user input
+async function getUserInput() {
+    const response = await enquirer.prompt({
+        type: "input",
+        name: "input",
+        message: "Enter your instruction:",
+    });
+    return response.input;
+}
+
+async function getCompletion(messages, requeryIncompletes = true) {
+    const conversation = {
+        model: 'gpt-4',
+        messages,
+        max_tokens: 2048,
+        temperature: 0.2,
+    }
+    let isJson = false, responseMessage = '';
+    const _query = async (conversation, iter) => {
+        const spinner = ora("Querying GPT-4").start();
+        let completion = await openai.createChatCompletion(conversation);
+        spinner.stop();
+        let response = await new Promise((resolve) => {
+            responseMessage += completion.data.choices[0].message.content.trim();
+            if (iter === 0 && responseMessage.startsWith('{') || responseMessage.startsWith('[')) {
+                isJson = true;
+            }
+            if (isJson && requeryIncompletes) {
+                if (responseMessage.endsWith('}') || responseMessage.endsWith(']')) {
+                    return resolve(responseMessage);
+                } else {
+                    conversation.messages.push({ role: 'assistant', content: response });
+                    responseMessage += completion;
+                    return resolve(_query(conversation, iter + 1));
+                }
+            } else return resolve(completion.data.choices[0].message.content.trim());
+        });
+        return responseMessage;
+    }
+    const completion = await _query(conversation, 0);
+    return completion;
+};
+
+// Update the file content and save it
+async function updateFile(file, newContent) {
+    const srcPath = path.join(file);
+    await fs.writeFile(srcPath, newContent, "utf-8");
+}
+
+async function getUserConfirmation() {
+    const response = await enquirer.prompt({
+        type: "confirm",
+        name: "confirmed",
+        message: "Do you want to apply the changes?",
+    });
+    return response.confirmed;
+}
+
+module.exports = {
+    loadFiles,
+    createConversation,
+    getUserInput,
+    getCompletion,
+    updateFile,
+    getUserConfirmation
+};
--- src/index.js
+++ src/index.js
@@ -1,264 +1,121 @@
 require("dotenv").config();
-
-const fs = require("fs-extra");
+const {
+    loadFiles,
+    createConversation,
+    getUserInput,
+    getCompletion,
+    updateFile,
+    getUserConfirmation,
+} = require("./code");
+const shell = require("shelljs");
 const path = require("path");
-const blessed = require("blessed");
-const contrib = require("blessed-contrib");
-const yargs = require("yargs");
 
-async function main() {
-    // the entire UI is nicely encapsulated in a single function
-    const jsonConfig = {
-        "titleBar": { type: "box", top: 0, left: 0, width: "100%", height: 1, content: 'Ratatoskr', style: { fg: "white", bg: "blue", } },
-        "fileList": { type: "list", top: 1, left: 0, width: "30%", height: "50%", label: "Files", border: { type: "line", }, style: { selected: { bg: "blue", }, }, keys: true, mouse: true, items: [], },
-        "outputLog": { type: "textarea", top: "52%", left: 0, width: "100%", height: "35%", label: "Output Log", content: "", scrollable: true, alwaysScroll: true, scrollbar: { ch: " ", inverse: true, }, border: { type: "line", }, keys: true, mouse: true },
-        "inputBox": { type: "textbox", bottom: 1, left: 0, width: "100%", height: 5, label: "[Input]", border: { type: "line", }, inputOnFocus: true, keys: true, mouse: true },
-        "statusBar": { type: "box", bottom: 0, left: 0, width: "100%", height: 1, content: "", style: { fg: "white", bg: "blue", }, },
-    }
-    const contribConfig = {
-        "fileContent": { type: "markdown", top: 1, left: "30%", width: "70%", height: "50%", label: "File Content", content: "", scrollable: true, alwaysScroll: true, scrollbar: { ch: " ", inverse: true, }, border: { type: "line", }, keys: true, mouse: true },
+(async () => {
+    // take a single path parameter as the value
+    const shellPath = process.argv[2];
+    let query = '';
+    if(process.argv.length > 3) {
+        query = process.argv.slice(3).join(' ');
     }
 
-    // Create a screen object
-    const terminal = blessed.screen({
-        smartCSR: true,
-        title: 'AI Assistant',
-    });
-    const [titleBar, fileList, outputLog, inputBox, statusBar,]
-        = Object.values(jsonConfig).map((config) => blessed[config.type](config)).map((element) => {
-            return element;
-        });
-    const [fileContent,] = Object.values(contribConfig).map((config) => contrib[config.type](config)).map((element) => {
-        return element;
-    });
-    [
-        titleBar, fileList, fileContent, outputLog, inputBox, statusBar,
-    ].forEach((element) => {
-        terminal.append(element);
-    });
-
-    // openai configuration
-    const { OpenAIApi, Configuration } = require('openai');
-    const configuration = new Configuration({ apiKey: process.env.OPENAI_KEY });
-    const openai = new OpenAIApi(configuration);
-    openai.apiKey = process.env.OPENAI_KEY;
-
-    // Load all files in the target folder  
-    function loadFiles(targetFolder) {
-        const files = (fs.readdirSync(targetFolder)).filter(file => fs.lstatSync(path.join(targetFolder, file)).isFile());
-        const fileContents = files.map(file => {
-            const content = fs.readFileSync(path.join(targetFolder, file), "utf-8");
-            return content.includes('ratatoskr:exclude') && file !== 'index.js' ? undefined : { name: file, content };
-        })
-        return fileContents.filter(file => file !== undefined);
+    let files = await loadFiles(shellPath);
+    if(query === '') {
+        console.log(`Loading files from ${shellPath}...`);
+        files.forEach((file) => console.log(`Loaded file ${file.name}`))
     }
+    let conversation = await createConversation(files);
 
-    // Create an OpenAI conversation with the contents of the loaded files
-    async function createConversation(files) {
-        // Add an initial message to inform the AI of its role and command capabilities
-        const initialMessage = {
-            role: "system",
-            content: `You are an AI assistant with regex-enabled search-and-replace functionality for modifying files. Your main role is to process and execute commands to edit the files. You are allowed to use control statements to guide the user or respond to user questions using the !echo statement.
-
-Control statement format:
-!edit <file> <search_pattern> <replacement>
-
-- <file>: The target file name.
-- <search_pattern>: The regex pattern to search for in the file, enclosed in quotes.
-- <replacement>: The replacement string, enclosed in quotes.
-
-Echo statement format:
-!echo <message>
-
-- <message>: The message to echo back to the user.
-
-RESPOND ONLY WITH CONTROL STATEMENTS OR ECHO STATEMENTS.`,
-        };
-        const messages = files.map((file) => ({
-            role: "system",
-            content: `The content of ${file.name}:
-${file.content}`,
-        }));
-        // Add the initial message to the conversation
-        messages.unshift(initialMessage);
-        messages.push({ role: "user", content: "ITS CRITICALLY IMPORTANT FOR YOU TO REMEMBER TO RESPOND ONLY WITH CONTROL STATEMENTS OR ECHO STATEMENTS! THIS IS ESSENTIAL!!" });
-        return messages;
+    // if query is passed from the command line, use it as the first user input
+    let requery = false;
+    if(query !== '') {
+        conversation.push({ role: "user", content: query });
+        requery = true;
     }
+    while (true) {
 
-    async function getCompletion(messages, requeryIncompletes = true) {
-        const conversation = { model: 'gpt-4', messages, max_tokens: 2048, temperature: 0.05 };
-        let isJson = false, responseMessage = '';
-        const _query = async (conversation, iter) => {
-            let completion = await openai.createChatCompletion(conversation);
-            responseMessage += completion.data.choices[0].message.content.trim();
-            if (iter === 0 && (responseMessage.startsWith('{') || responseMessage.startsWith('['))) isJson = true;
-            if (isJson && requeryIncompletes && !(responseMessage.endsWith('}') || responseMessage.endsWith(']'))) {
-                conversation.messages.push({ role: 'assistant', content: responseMessage });
-                return _query(conversation, iter + 1);
-            }
-            return responseMessage;
+        // only gather user input if we are not requerying
+        if(!requery) {
+            const userInput = await getUserInput();
+            if (userInput === '~' || userInput === '!exit'
+            ) break;
+            conversation.push({ role: "user", content: userInput });
+            // TODO - remove oldest messages if conversation is too long
         }
-        return await _query(conversation, 0);
-    }
-
-    // Update the file content and save it
-    async function updateFile(file, newContent) {
-        const srcPath = path.join(__dirname, file);
-        fs.writeFileSync(srcPath, newContent, "utf-8");
-    }
-
-    let conversation;
-
-    async function getUserConfirmation(screen, message = 'Are you sure you want to proceed?', onYes, onNo) {
-        // Create a box element for the dialog
-        const dialog = blessed.box({
-            top: 'center',
-            left: 'center',
-            width: '50%',
-            height: 'shrink',
-            tags: true,
-            keys: true,
-            border: { type: 'line' },
-            label: 'Confirmation',
-        });
-
-        // Set the content of the dialog
-        dialog.setContent(message);
-
-        // Create Yes and No buttons
-        const yesButton = blessed.button({
-            mouse: true,
-            keys: true,
-            shrink: true,
-            padding: { left: 1, right: 1 },
-            left: '25%',
-            top: 2,
-            tags: true,
-            name: 'yes',
-            content: 'Yes',
-            style: { focus: { inverse: true } },
-        });
-
-        const noButton = blessed.button({
-            mouse: true,
-            keys: true,
-            shrink: true,
-            padding: { left: 1, right: 1 },
-            left: '50%',
-            top: 2,
-            tags: true,
-            name: 'no',
-            content: 'No',
-            style: { focus: { inverse: true } },
-        });
-
-        // Add event listeners for the buttons
-        yesButton.on('press', () => {
-            dialog.detach();
-            screen.render();
-            onYes();
-        });
-
-        noButton.on('press', () => {
-            dialog.detach();
-            screen.render();
-            onNo();
-        });
-
-        // Add the buttons to the dialog box
-        dialog.append(yesButton);
-        dialog.append(noButton);
-
-        // Focus on the Yes button by default
-        yesButton.focus();
-
-        // Add the dialog box to the screen
-        screen.append(dialog);
-
-        // Render the screen
-        screen.render();
-    }
-
-    const argv = yargs.argv;
-
-    let targetFolder = argv._[0];
-    if (!targetFolder) {
-        console.error("Please provide a target folder as the first argument.");
-        process.exit(1);
-    }
-    let files = await Promise.all(loadFiles(targetFolder));
-
-    // Load the files and populate the fileList component
-    const loadScreen = async () => {
-        files = loadFiles(targetFolder);
-        const fileNames = files.map((file) => file.name);
-        fileList.setItems(fileNames);
-        conversation = !conversation ? await createConversation(files) : conversation;
-        terminal.render();
-    };
-    loadScreen();
-
-
-    inputBox.on("submit", async (value) => {
-        outputLog.insertBottom(`User: ${value}`);
-        terminal.render();
-
-        conversation.push({ role: "user", content: value });
         const completion = await getCompletion(conversation);
-        outputLog.insertBottom(`AI: ${completion}`);
-        terminal.render();
 
-        const editCommands = completion.matchAll(/!edit\s+"((?:\\"|[^"])*)"\s+"((?:\\"|[^"])*)"\s+"((?:\\"|[^"])*)"/g);
+        console.log("AI Response:", completion);
+        // Process the edit commands
+        const editCommands = completion.matchAll(/!edit\s+(\S+)\s+"([^"]+)"\s+"([^"]+)"/g);
+        const echoCommands = completion.matchAll(/!echo\s+"([^"]+)"/g);
+        const bashCommands = completion.matchAll(/!bash\s+"([^"]+)"/g);
+        const successCommands = completion.matchAll(/!success\s+"([^"]+)"/g);
+        const failureCommands = completion.matchAll(/!failure\s+"([^"]+)"/g);
+        
+        for(const command of successCommands) {
+            requery = false;
+        }
+        for(const command of failureCommands) {
+            requery = false
+        }
+        for(const command of echoCommands) {
+            const [_, message] = command;
+            console.log(message);
+        }
+        // Group the updates by file name
         const updates = {};
         for (const command of editCommands) {
-            const [_, fileName, searchPattern, replacement] = command;
+            let [_, fileName, searchPattern, replacement] = command;
+            // strip doublequotes from file name
+            fileName = fileName.replace(/^"(.*)"$/, "$1");
             const file = files.find((f) => f.name === fileName);
             if (file) {
                 const regex = new RegExp(searchPattern, "g");
-                // we need to remove the backslashes from the replacement string but save the newlines
-                const replacement = command[3].replace(/\\n/g, "\n").replace(/\\(.)/g, "$1");
                 const newContent = file.content.replace(regex, replacement);
                 updates[fileName] = newContent;
+            } else {
+                // create a new file
+                updates[fileName] = replacement;
             }
         }
-        try {
-            for (const fileName of Object.keys(updates).sort()) {
-                await getUserConfirmation(terminal, `Do you want to apply the following changes to ${fileName}?
-                ${updates[fileName]}
-                `, async () => {
-                    const file = files.find((f) => f.name === fileName);
-                    file.content = updates[fileName];
-                    await updateFile(fileName, updates[fileName]);
-                    outputLog.insertBottom(`File ${fileName} updated successfully.`);
-                    terminal.render();
-                }, () => {
-                    outputLog.insertBottom(`Changes to ${fileName} were not applied.`);
-                    terminal.render();
-                });
+
+        // Show the updates to the user and wait for confirmation
+        for (let fileName of Object.keys(updates).sort()) {
+            console.log(`\nUpdated content for ${fileName}:\n${updates[fileName]}`);
+            const confirmed = await getUserConfirmation();
+            if (confirmed) {
+                const file = files.find((f) => fileName === '"' +  f.name + '"' || f.name === fileName);
+                file.content = updates[fileName];
+                const cwd = process.cwd();
+                const filePath = path.join(cwd, shellPath, fileName);
+                await updateFile(filePath, updates[fileName]);
+                console.log(`File ${filePath} updated successfully.`);
+            } else {
+                console.log(`Changes to ${fileName} were not applied.`);
             }
-        } catch (e) {
-            console.error(e);
-        } finally {
-            process.stdin.resume();
         }
 
-    });
+        for (const command of bashCommands) {
+            const [_, bashCommand] = command;
+            const { stdout, stderr } = shell.exec(bashCommand);
+            if (stderr) {
+                console.log(stderr);
+                conversation.push({ role: "system", content: stderr });
+                requery = true;
+            } else {
+                console.log(stdout);
+                conversation.push({ role: "system", content: stdout });
+                requery = true;
+            }
+        }
 
-    // Update event listeners and interaction logic
-    fileList.on("select", async (item) => {
-        const fileName = item.content;
-        const file = files.find((f) => f.name === fileName);
-        fileContent.setMarkdown(`\`\`\`javascript\n${file.content}\n\`\`\``);
-        statusBar.setContent(`Status: Selected file: ${fileName} | Updates applied: 0`);
-        terminal.render();
-    });
+        for (const command of successCommands) {
+            const [_, successMessage] = command;
+            console.log(successMessage);
+        }
 
-    fileList.focus();
-    terminal.render();
-    terminal.key(['q', 'C-c'], () => process.exit(0));
-    terminal.key(['/'], () => {
-        // placeholder - we will add a popup box here to gather a new target folder
-        targetFolder = blessed.helpers.escape(targetFolder);
-    });
-}
-main();
+        for (const command of failureCommands) {
+            const [_, failureMessage] = command;
+            console.log(failureMessage);
+        }
+    }
+})();

ab23ac5c8805971a33c0fd112526db666b476582
Sebastian Schepis <sschepis@gmail.com>
2023-04-27 12:29:08 -0700

    new objects, docs, stuff

--- src/cli.js
+++ src/cli.js
@@ -37,7 +37,8 @@ const { message } = require("blessed");
             content: `You are a master developer skilled in bash commands, task decomposition, app design and implementation. Given user input, you either implement the task in one-shot or you decompose the task then implement the sub-tasks.
 1. No natural-language responses or commentary. ALL COMMANDS ON THEIR OWN LINE
 2. Complete task, then output /TASK <taskname>.
-3. ||Decompose tasks into bash commands||, output each task with [ ] preceeding it
+3. ||Decompose tasks into bash commands||, output each task with [ ] preceeding it. 
+4. Append /DECOMPOSITION after decomposing tasks
 4. Request stdout data with /SHOWTERMINAL ON ITS OWN LINE
 5. Use cat, tail, echo, sed, and unified diff to manipulate files.
 6. Ask questions with /ASK <question> on its own line
@@ -53,7 +54,7 @@ const { message } = require("blessed");
         let iterations = 0, pendingOutput = [
         ];
         log(`Starting software developer with ${messages.length} messages in ${path}`);
-        let potentialBashStatements = [];
+        let potentialBashStatements = [], tasksList = [];
         while(iterations < maxIterations) {
             const spinner = ora("Querying GPT-4").start();
             let result = await getCompletion(messages, {
@@ -95,6 +96,13 @@ const { message } = require("blessed");
                         case 'TASK':
                             const taskName = statement.split(' ').slice(1).join(' ');
                             pendingOutput.push(`[x] ${taskName}`);
+
+                            messages.push({
+                                role: "assistant",
+                                content: pendingOutput.join('\n') 
+                                + '\TASK'
+                            })
+
                             break;
 
                         case 'SHOWTERMINAL':
@@ -106,6 +114,19 @@ const { message } = require("blessed");
                             break;
 
                         case 'DECOMPOSED':
+                            // get the first incomplete task
+                            tasksList = pendingOutput.filter((line)=> line.startsWith('[ ]'));
+                            const incompleteTask = pendingOutput.filter((line)=> line.startsWith('[ ]'))[0];
+                            messages.push({
+                                role: "assistant",
+                                content: pendingOutput.join('\n') 
+                                + '\nDECOMPOSED'
+                            })
+                
+                            messages.push({
+                                role: "user",
+                                content:'Complete the task: ' + incompleteTask
+                            })
                             break;
 
                         case 'ASK':
@@ -146,12 +167,6 @@ const { message } = require("blessed");
                     }
                 }
             }
-
-            messages.push({
-                role: "assistant",
-                content: pendingOutput.join('\n')
-            })
-
 
             if(isDone) {
                 log('Done');
